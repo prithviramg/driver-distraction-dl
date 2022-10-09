a7ae2b051d53e28f7057bc7179868b551e4b8430
prithviramg <54787409+prithviramg@users.noreply.github.com>
2022-10-09 11:39:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if uploaded_file is not None:
     print(y.shape)
     y1, y2 = np.argsort(y)[::-1][:2]
     with col2:
-        st.write(f"Action performed by the driver - \"{y}\"")
+        st.write(f"Action performed by the driver - \"{y.shape}\"")
         st.write(f"time taken to find the Action - {(end-start)*10**3:.03f}ms")
 
 
